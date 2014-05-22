8f7f27c12ca18fc065f2c3c23e5e79ba6e51e9e8
Leonardo Ferreyra <leferreyra@gmail.com>
2014-05-21 21:58:51 -0300

    Refactor directories for newer Django version and use virtualenv

--- jornadasutn/urls.py
+++ jornadasutn/urls.py
@@ -6,5 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$','principal.views.home'),
+    url(r'^$','website.views.home'),
 )
